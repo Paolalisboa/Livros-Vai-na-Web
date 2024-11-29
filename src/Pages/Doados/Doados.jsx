239fc1859ef776a62e35cb9706fc17f160dead15
Paolalisboa <lisboa.paola3@gmail.com>
2024-11-29 20:57:07 -0300

    Adicionei a página de Livros Doados

--- src/Pages/Doados/Doados.jsx
+++ src/Pages/Doados/Doados.jsx
@@ -1,10 +1,24 @@
+import S from './doados.module.scss'
+import livro from '../../assets/livro.png'
 
-
-
-export default function Doados (){
+export default function Doados(){
     return(
-        <section>
-            <h2>Doados</h2>
+    <section className={S.boxDoados}>
+            <h2>Livros Doados</h2>
+       <section className={S.boxBooks}>
+           <article>
+            <img src={livro} alt="imagem do livro o prota"/>
+            <h4>O protagonista</h4>
+            <h5>Susanne Andrade</h5>
+            <h5>Ficção</h5>
+          </article>
+          {/* <div className={S.box}>
+            <img src={livro} alt="" />
+            <h4>O protagonista</h4>
+            <p>Susanne Andrade</p>
+            <p>Ficção</p>
+          </div> */}
         </section>
+    </section>
     )
 }
